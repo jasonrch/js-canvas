651dc3780ec73627b736ad11ab7308bce99f2884
jasonrch <151870228+jasonrch@users.noreply.github.com>
2019-04-04 12:03:17 -0500

    Add files via upload

---
+++ canvas.js
@@ -0,0 +1,24 @@
+var canvas = document.querySelector('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+
+var x=200;
+
+function animate() {
+    c.clearRect(0, 0, innerWidth, innerHeight);
+    
+    requestAnimationFrame(animate);
+    c.beginPath();
+    c.arc(x, 200, 30, 0, Math.PI * 2, false);
+    c.strokeStyle = 'blue';
+    c.stroke();
+    
+    x +=1;
+    }
+    
+
+animate();
+console.log(canvas);
